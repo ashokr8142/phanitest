48b96eb9ee27b3028ab9ab4c4d50598bf31aea31
Ola Spyra <aspy@google.com>
2020-04-01 20:05:00 -0700

    Firebase function for exporting all collection under root.
    The export is triggered at 2am PST daily.
    
    Change-Id: I19ef37d257fdc7d55b9c93db53f0ffc378b4da0b

---
+++ firestore/raw_data_export/functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+exports.scheduledFirestoreExport = functions.pubsub.schedule(
+  'every day 02:00').onRun(
+    async context => {
+  // Get all colletion ids under root.
+  const collections = await admin.firestore().listCollections();
+  const collectionIds = collections.map(c => c.path);
+
+  const databaseName = client.databasePath(
+    process.env.GCLOUD_PROJECT,
+    '(default)'
+  );
+  const outputBucketPath = 'gs://heroes-hat-firestore-raw'
+  const operationInfo = await admin.firestore().exportDocuments(
+    {databaseName, collectionIds, outputUriPrefix: outputBucketPath});
+  console.log(operationInfo);
+};
