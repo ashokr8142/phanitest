e6ae866b9709ed4792f8790fa9ed4684c6bb162e
Ola Spyra <aspy@google.com>
2020-04-20 19:08:40 -0700

    Publish firestore triggers data to newly created topic projects/heroes-hat-dev/topics/surveyWriteTrigger.
    Change-Id: I347de6be10163f600d8b1db52923daf224db191c

--- firestore/real_time_triggers/functions/index.js
+++ firestore/real_time_triggers/functions/index.js
@@ -1,7 +1,11 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const {PubSub} = require('@google-cloud/pubsub');
+const pubSubClient = new PubSub();
 
 admin.initializeApp();
+
+const globalTopicName = 'projects/heroes-hat-dev/topics/surveyWriteTrigger';
 
 exports.surveyWriteTrigger = functions.firestore
   .document('{studyResponses}/{studyId}/Activities/{activityId}')
@@ -22,12 +26,35 @@ exports.surveyWriteTrigger = functions.firestore
   });
 
 const getStudyHandler = (studyId) => {
-  // For now we only log, no matter the study.
-  return logInfo;
+  // For now we push to common global topic, no matter the study.
+  return publishToGlobalTopic;
 }
 
 const logInfo = (studyCollection, studyId, activityId, data) => {
   console.log(
     `Change in study ${studyId}, activity ${activityId},
     participant ${data.participantId}`);
+}
+
+const publishToGlobalTopic = (
+  studyCollection,studyId, activityId, data) => {
+  // For now wire whole response data, we may want to change it to collect
+  // statistics first.
+  const message = {
+    studyCollection: studyCollection,
+    studyId: studyId,
+    activityId: activityId,
+    response: data,
+  };
+  const dataBuffer = Buffer.from(JSON.stringify(message));
+
+  pubSubClient.topic(globalTopicName).publish(dataBuffer).then(messageId => {
+        console.log(`Message ${messageId} published.`);
+        return 0;
+      })
+      .catch(err => {
+        console.error('Pulish message error :', err);
+        return 0;
+      });
+  logInfo(studyCollection, studyId, activityId, data);
 }
