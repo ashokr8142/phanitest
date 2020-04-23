520f0b0a59dc12e838d1cc43c2a8d54b2a70a7a9
Qiumin Xu <qiuminxu@google.com>
2020-04-22 22:27:41 -0700

    Update branding tweaks.
    Change-Id: I575c14f03a2d3b3084ca1c043d9066629e4e70c8

--- Android/app/src/main/java/com/harvard/usermodule/SignInActivity.java
+++ Android/app/src/main/java/com/harvard/usermodule/SignInActivity.java
@@ -131,7 +131,7 @@ public class SignInActivity extends AppCompatActivity implements ApiCall.OnAsync
   private void customTextView() {
     String html =
         getResources().getString(R.string.new_user)
-            + " <font color=\"#007cba\">"
+            + " <font color=\"#2f6942\">"
             + getResources().getString(R.string.side_menu_sign_up)
             + "</font>";
     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
