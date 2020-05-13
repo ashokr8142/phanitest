25d9d59df1a2ee6d66a20f05d1a947ec3f09ae7b
Ola Spyra <aspy@google.com>
2020-05-12 19:10:58 -0700

    Populate personalized_user_report.
    Change-Id: I32d1e7a1c4c641e36aafdb3d9eaad184594a8ec7

--- firestore/real_time_triggers/subscriber/subscriber.py
+++ firestore/real_time_triggers/subscriber/subscriber.py
@@ -1,4 +1,5 @@
 import abc
+import datetime
 import json
 import logging
 import os
@@ -18,6 +19,8 @@ _DB_NAME = os.environ.get("DB_NAME")
 _EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
 _EMAIL_PASS = os.environ.get("EMAIL_PASS")
 _SQL_CONNECTION = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
+# Surveys that should trigger user report update.
+_REPORT_SURVEYS = set(os.environ.get('USER_REPORT_SURVEYS').split(','))
 
 
 class EmailSender(object):
@@ -50,6 +53,14 @@ class Subscriber(object):
       project_id, subscription_name)
     self.activity_id = activity_id
     self.user_report = user_report.UserReportHandler()
+    self.db = sqlalchemy.create_engine(
+      sqlalchemy.engine.url.URL(
+        drivername="mysql+pymysql",
+        username=_DB_USER,
+        password=_DB_PASS,
+        database=_DB_NAME))
+        # To run with local cloud proxy instance add query to the method above:
+        # query={"unix_socket": "/cloudsql/{}".format(_SQL_CONNECTION)}
 
   def get_streaming_subscription(self):
     """Retruns streaming subscription on Google Pub/Sub topic."""
@@ -58,9 +69,71 @@ class Subscriber(object):
       callback=self.get_callback())
     return subscription
 
-  def _update_user_report(self, participant_id):
-    # For now just print the content.
-    print(self.user_report.create_report_html(participant_id))
+  def _get_user_details_id(self, participant_id):
+    with self.db.connect() as conn:
+      query = sqlalchemy.text(
+        '''SELECT user_details_id FROM participant_study_info
+         WHERE participant_id=:participant_id''')
+      result = conn.execute(query, participant_id=participant_id).fetchone()
+      if not result:
+        raise ValueError(
+          'Failed fetching user_id for participant {}'.format(
+            participant_id))
+      return result[0]
+
+  @abc.abstractmethod
+  def get_callback(self):
+    """Defines callback invoked by the subscription."""
+
+@six.add_metaclass(abc.ABCMeta)
+class SubscriberWithUserReport(Subscriber):
+  def __init__(self, project_id, subscription_name, activity_id=None):
+     super(SubscriberWithUserReport, self).__init__(project_id,
+                                                    subscription_name,
+                                                    activity_id)
+
+  def update_user_report(self, participant_id, study_id):
+    try:
+      user_id = self._get_user_details_id(participant_id)
+      with self.db.connect() as conn:
+        activity_date_time = datetime.datetime.now()
+        # The title contains date of the last Monday from the activity date.
+        report_date = (activity_date_time - datetime.timedelta(
+          days=activity_date_time.weekday())).date()
+        report_title = 'Summary Report: Week of {} {}, {}'.format(
+          report_date.strftime("%B"), report_date.day, report_date.year)
+        report_content = self.user_report.create_report_html(participant_id)
+        study_info_id = self._get_study_id(study_id)
+        insert_query = sqlalchemy.text(
+          '''INSERT INTO personalized_user_report
+              (activity_date_time, report_content, report_title, study_info_id,
+              user_id)
+            VALUES (:activity_date_time, :report_content, :report_title,
+              :study_info_id, :user_id)
+            ON DUPLICATE KEY UPDATE activity_date_time = :activity_date_time,
+              report_content = :report_content, report_title = :report_title,
+              study_info_id = :study_info_id, user_id = :user_id''')
+        conn.execute(
+          insert_query,
+          activity_date_time = activity_date_time,
+          report_content=report_content, report_title=report_title,
+          study_info_id=study_info_id, user_id=user_id)
+    except Exception as e:
+      raise ValueError(
+        '''Failed upsert into personalized_user_report table for
+          participant {} study_id {}, exception {}'''.format(
+            participant_id, study_id, e))
+
+  def _get_study_id(self, study_name):
+    with self.db.connect() as conn:
+      query = sqlalchemy.text(
+        '''SELECT id FROM study_info WHERE name=:study_name''')
+      result = conn.execute(query, study_name=study_name).fetchone()
+      if not result:
+        raise ValueError(
+          'Failed fetching study id for study name {}'.format(
+            study_name))
+      return result[0]
 
   @abc.abstractmethod
   def get_callback(self):
@@ -84,12 +157,14 @@ class TestSubscriber(Subscriber):
 class RoutingSubscriber(Subscriber):
   """Delegates work to subsubscribers or messages without explicit suscriber."""
   def __init__(self, project_id, subscription_name):
-     # Map from activityId to subscriber.
-     self.subscriber_map = {
-      'PHQDepWithSum': PHQDepSubscriber(project_id, subscription_name),
+    # Map from activityId to subscriber
+    self.subscriber_map = {
       'Demographics': DemographicsSubscriber(project_id, subscription_name)
-     }
-     super(RoutingSubscriber, self).__init__(project_id, subscription_name)
+    }
+    for activity_id in _REPORT_SURVEYS:
+      self.subscriber_map[activity_id] = HealthActivitySubscriber(
+        project_id, subscription_name, activity_id)
+    super(RoutingSubscriber, self).__init__(project_id, subscription_name)
 
   def get_callback(self):
 
@@ -108,14 +183,6 @@ class RoutingSubscriber(Subscriber):
 class DemographicsSubscriber(Subscriber):
   """Subsctiber fro Demographics survey massages."""
   def __init__(self, project_id, subscription_name):
-    self.db = sqlalchemy.create_engine(
-      sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=_DB_USER,
-        password=_DB_PASS,
-        database=_DB_NAME))
-        # To run with local cloud proxy instance add query to the method above:
-        # query={"unix_socket": "/cloudsql/{}".format(_SQL_CONNECTION)}
     super(DemographicsSubscriber, self).__init__(project_id, subscription_name,
                                                  'Demographics')
 
@@ -129,12 +196,12 @@ class DemographicsSubscriber(Subscriber):
           if result['key'] == 'HealthcareSys':
             institution_id = result['value']
         participant_id = data['participantId']
+        study_id = data['data']['studyId']
         print(
           '''Received Demogrphics message from participant {}
             with institution {}'''.format(participant_id, institution_id))
         try:
           self._insert_user_institution(participant_id, institution_id)
-          self._update_user_report(participant_id)
           message.ack()
         except Exception as e:
           print(str(e))
@@ -148,71 +215,57 @@ class DemographicsSubscriber(Subscriber):
     return _callback
 
   def _insert_user_institution(self, participant_id, institution_id):
-    with self.db.connect() as conn:
-      query = sqlalchemy.text(
-        '''SELECT user_details_id
-         FROM participant_study_info
-         WHERE participant_id=:participant_id''')
-      result = conn.execute(query, participant_id=participant_id).fetchone()
-      if not result:
-        raise ValueError(
-          'Failed fetching user_details_id for participant {}'.format(
-            participant_id))
-      user_details_id = result[0]
-      insert_query = sqlalchemy.text(
-        '''INSERT INTO user_institution (user_details_id, institution_id)
-          VALUES (:user_details_id, :institution_id)
-          ON DUPLICATE KEY UPDATE institution_id = :institution_id''')
-      try:
+    try:
+      user_details_id = self._get_user_details_id(participant_id)
+      with self.db.connect() as conn:
+        insert_query = sqlalchemy.text(
+          '''INSERT INTO user_institution (user_details_id, institution_id)
+            VALUES (:user_details_id, :institution_id)
+            ON DUPLICATE KEY UPDATE institution_id = :institution_id''')
         conn.execute(
           insert_query,
           user_details_id=user_details_id,
           institution_id=institution_id)
-      except Exception as e:
-        raise ValueError(
-          '''Failed upsert into user_institution table for participant {}
-           institution {}, exception {}'''.format(participant_id,
-                                                  institution_id, e))
+    except Exception as e:
+      raise ValueError(
+        '''Failed upsert into user_institution table for participant {}
+         institution {}, exception {}'''.format(participant_id,
+                                                institution_id, e))
 
-class PHQDepSubscriber(Subscriber):
-  """Subscriber for PHQDep survey messages."""
-  def __init__(self, project_id, subscription_name):
-    self.db = sqlalchemy.create_engine(
-      sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=_DB_USER,
-        password=_DB_PASS,
-        database=_DB_NAME))
-        # To run with local cloud proxy instance add query to the method above:
-        # query={"unix_socket": "/cloudsql/{}".format(_SQL_CONNECTION)}
-    self.email_sender = EmailSender()
-    super(PHQDepSubscriber, self).__init__(project_id, subscription_name,
-                                           'PHQDepWithSum')
+class HealthActivitySubscriber(SubscriberWithUserReport):
+  """Subscriber for activity survey messages."""
+  def __init__(self, project_id, subscription_name, activity_id):
+    # If you want to send an email on the survey completion uncomment this line.
+    # Then use self._send_email in callback.
+    # self.email_sender = EmailSender()
+    super(HealthActivitySubscriber, self).__init__(
+      project_id, subscription_name, activity_id)
 
   def get_callback(self):
 
     def _callback(message):
       data = json.loads(message.data)
       if data['activityId'] == self.activity_id:
-        score_sum = 0.0
-        for result in data['data']['results']:
-          if result['key'] == '_SUM':
-            score_sum = score_sum + float(result['value'])
         participant_id = data['participantId']
-        email = self._get_participant_email(participant_id)
-        email_text = 'Your last PHQ survey score: {}.'.format(score_sum)
+        study_id = data['data']['studyId']
         print(
-          'Received PHQDep message from participant {} with score {}'.format(
-            participant_id, score_sum))
-        if email:
-          self.email_sender.send('PHQ survey result.', email_text, [email])
-        self._update_user_report(participant_id)
+          'Received {} message from participant {}'.format(
+            self.activity_id, participant_id))
+        self.update_user_report(participant_id, study_id)
         message.ack()
       else:
         print('Ignoring message from activity {}'.format(data['activityId']))
         message.nack()
 
     return _callback
+
+  def _send_email(score_sum):
+    email = self._get_participant_email(participant_id)
+    if email:
+      email_text = 'Your last {} survey score: {}.'.format(
+        self.activity_id, score_sum)
+      self.email_sender.send('{} survey result.'.format(self.activity_id),
+                             email_text, [email])
 
   def _get_participant_email(self, participant_id):
     with self.db.connect() as conn:
--- firestore/real_time_triggers/subscriber/user_report.py
+++ firestore/real_time_triggers/subscriber/user_report.py
@@ -11,67 +11,82 @@ _PROJECT_NAME = os.environ.get('PROJECT_ID')
 _REPORT_SURVEYS = set(os.environ.get('USER_REPORT_SURVEYS').split(','))
 _SURVEYS_COLLECTION_PREFIX = os.environ.get('SURVEYS_COLLECTION_PREFIX')
 
-ActivityMsgs = namedtuple('ActivityMsgs', 'ranges msgs')
+ActivityMsgs = namedtuple('ActivityMsgs', 'title ranges text_colors')
+TextAndColor = namedtuple('TextAndColor', 'text color')
+
+# The app needs to be initialized only once, therefor intialize globally.
+cred = credentials.ApplicationDefault()
+# For local debugging, use certificate.
+# cred = credentials.Certificate('/path/to/firestore/key.json')
+firebase_admin.initialize_app(cred, { 'projectId': _PROJECT_NAME, })
+
+_COLORS = {
+  -1: '#FFFFFF', # white
+  0: '#CEE0D4', # green
+  1: '#FFF2CC', # yellow
+  2: '#FBE5D6', # light red
+  3: '#EAB0AB', # red
+  4: '#EA8279' # dark red
+}
 
 # TODO: This should be read from some kind of config file.
 _ACTIVITY_ID_TO_MSG = {
-  'PHQ-9': ActivityMsgs([0, 5, 10, 15, 20],
-                        ['Minimal or No Symptoms',
-                         'Mild Symptoms',
-                         'Moderate Symptoms',
-                         'Moderately Severe Symptoms',
-                         'Severe Symptoms']),
-  'GAD-7': ActivityMsgs([0, 5, 10, 15],
-                        ['Minimal or No Symptoms',
-                         'Mild Symptoms',
-                         'Moderate Symptoms',
-                         'Severe Symptoms']),
-  'PSQI': ActivityMsgs([0, 1, 2, 3],
-                       ['Very Good',
-                        'Fairly Good',
-                        'Fairly Bad',
-                        'Very Bad']),
-  'PCL-5': ActivityMsgs([0, 5],
-                        ['Minimal or No Symptoms',
-                         'Significant Symptoms']),
-  'PHQDepWithSum': ActivityMsgs([0, 5, 10, 15, 20],
-                        ['Minimal or No Symptoms',
-                         'Mild Symptoms',
-                         'Moderate Symptoms',
-                         'Moderately Severe Symptoms',
-                         'Severe Symptoms'])
+  'PSQI2': ActivityMsgs('Your sleep quality was',
+                        [0, 1, 2, 3],
+                        [TextAndColor('Very Good', _COLORS[0]),
+                         TextAndColor('Fairly Good', _COLORS[1]),
+                         TextAndColor('Fairly Bad', _COLORS[2]),
+                         TextAndColor('Very Bad', _COLORS[4])]),
+  'PHQ9+SH': ActivityMsgs('Your feelings of sadness or depression were',
+                          [0, 5, 10, 15, 20],
+                          [TextAndColor('None or minimal', _COLORS[0]),
+                           TextAndColor('Mild', _COLORS[1]),
+                           TextAndColor('Moderate', _COLORS[2]),
+                           TextAndColor('Moderately Severe', _COLORS[3]),
+                           TextAndColor('Severe', _COLORS[4])]),
+  'GAD7': ActivityMsgs('Your feelings of worry or anxiety were',
+                       [0, 5, 10, 15],
+                       [TextAndColor('None or minimal', _COLORS[0]),
+                        TextAndColor('Mild', _COLORS[1]),
+                        TextAndColor('Moderate', _COLORS[2]),
+                        TextAndColor('Severe', _COLORS[4])]),
+  'PTSD2': ActivityMsgs('Symptoms of posttraumatic stress were',
+                        [0, 6],
+                        [TextAndColor('None or minimal', _COLORS[0]),
+                         TextAndColor('Present', _COLORS[2])]),
+  'WSAS2': ActivityMsgs('''Your limitations in day-to-day life due to your
+                        mental health were''',
+                        [0, 10, 21],
+                        [TextAndColor('None or mild', _COLORS[0]),
+                         TextAndColor('Moderate', _COLORS[2]),
+                         TextAndColor('Severe', _COLORS[4])])
 }
 
 def get_message_from_score(activity_id, score):
   if score is None:
-    return 'Survey not completed'
+    return TextAndColor('Survey not completed', _COLORS[-1])
   if activity_id not in _ACTIVITY_ID_TO_MSG:
-    return 'Unknown survey'
+    return TextAndColor('Unknown survey', _COLORS[-1])
   index = bisect_right(_ACTIVITY_ID_TO_MSG[activity_id].ranges, score)
   if index > 0:
     index -= 1
-  return _ACTIVITY_ID_TO_MSG[activity_id].msgs[index]
+  return _ACTIVITY_ID_TO_MSG[activity_id].text_colors[index]
 
-def html_table_line(title, value):
+def html_table_line(title, text_and_color):
   return '''<tr style="border:1px solid black;">
       <td>{}</td>
-      <td>{}</td></tr>'''.format(title, value)
+      <td style="background-color:{}">{}</td></tr>'''.format(
+        title, text_and_color.color, text_and_color.text)
 
 class UserReportHandler(object):
 
   def __init__(self):
-    self.cred = credentials.ApplicationDefault()
-    # For local debugging, use certificate.
-    # self.cred = credentials.Certificate('/path/to/firestore/key.json')
-    firebase_admin.initialize_app(self.cred, {
-      'projectId': _PROJECT_NAME,
-    })
     self.firebase_db = firestore.client()
 
   def create_report_html(self, participant_id):
     result = '<table style="border:1px solid black;border-collapse: collapse;">'
     for key, value in self.get_updated_score_map(participant_id).items():
-      result += html_table_line(key, value)
+      result += html_table_line(_ACTIVITY_ID_TO_MSG[key].title, value)
     result += '</table>'
     return result
 
