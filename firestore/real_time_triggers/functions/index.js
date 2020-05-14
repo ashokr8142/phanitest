63dda6c2a50cfff361ea4f7302c3f4b5b622877d
Ola Spyra <aspy@google.com>
2020-05-14 12:24:34 -0700

    Fighting the timeout bug.
    Change-Id: I09641ebe1ddc3b4e70b46411d83887a7e0455d0f

--- firestore/real_time_triggers/functions/index.js
+++ firestore/real_time_triggers/functions/index.js
@@ -13,48 +13,24 @@ const ignoreValue = -999;
 
 exports.surveyWriteTrigger = functions.firestore
   .document('{studyResponses}/{studyId}/Activities/{activityUid}')
-  .onWrite((change, context) => {
+  .onWrite(async (change, context) => {
     // Contains the data that was written.
-    const data = change.after.data()
+    const data = change.after.data();
     // Activity string id, e.g. PHQDep.
     const activityId = data.activityId;
     // Participant id identifies the user.
     const participantId = data.participantId;
     // Get handler for the study.
     try {
-      const handler = getActivityHandler(activityId);
-      handler(activityId, participantId, data);
+      await publishToGlobalTopic(activityId, participantId, data);
     } catch(error) {
       console.error(error);
+      return -1;
     }
     return 0;
   });
 
-const getActivityHandler = (activityId) => {
-  // For now we push raw data for any other study.
-  return publishToGlobalTopic;
-}
-
-const publishResponseValuesSum = (activityId, participantId, data) => {
-  if (data.results.length === 0 || data.results[0].length === 0) {
-    console.log(`Data empty for user ${participantId} activity ${activityId}.`)
-    return;
-  }
-  let scoreSum = 0;
-  const times = [];
-  response = data.results[0];
-  for (let [index, question_response] of response) {
-    if (question_response.score !== ignoreValue) {
-      scoreSum = scoreSum + question_response.value;
-      times.push(new Date(quesiton_response.endTime));
-    }
-  }
-  publishToGlobalTopic(activityId, participantId, {
-    individualResponseScoreSum: scoreSum,
-    actvityEndTime: Math.max([new Date(0), Math.max(times)]).ToString()});
-}
-
-const publishToGlobalTopic = (activityId, participantId, data) => {
+async function publishToGlobalTopic(activityId, participantId, data) {
   // Wire the data with activityId.
   const message = {
     activityId: activityId,
@@ -64,15 +40,14 @@ const publishToGlobalTopic = (activityId, participantId, data) => {
   };
   const dataBuffer = Buffer.from(JSON.stringify(message));
 
-  pubSubClient.topic(globalTopicName).publish(dataBuffer).then(messageId => {
-        console.log(`Message ${messageId} published.`);
-        logInfo(activityId, participantId, data);
-        return 0;
-      })
-      .catch(err => {
-        console.error('Pulish message error :', err);
-        return 0;
-      });
+  try {
+    const messageId = await pubSubClient.topic(
+      globalTopicName).publish(dataBuffer);
+    console.log(`Message ${messageId} published.`);
+    logInfo(activityId, participantId, data);
+  } catch(err) {
+    console.error('Pulish message error :', err);
+  }
 }
 
 const logInfo = (activityId, participantId, data) => {
