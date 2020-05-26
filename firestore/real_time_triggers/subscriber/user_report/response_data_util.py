2aae0ea53a26eafc7345b73b9b3b9d84c93bb55f
Yu-chi Kuo <yuchikuo@google.com>
2020-05-26 16:13:27 -0700

    Add util function to group survey response by week.
    Change-Id: Iabab131268e1f1a99b1d594d397f8504bfbea921

--- firestore/real_time_triggers/subscriber/user_report/response_data_util.py
+++ firestore/real_time_triggers/subscriber/user_report/response_data_util.py
@@ -1,4 +1,5 @@
 """Util functions for processing Firestore response data."""
+from collections import namedtuple
 import datetime
 import pytz
 
@@ -34,3 +35,40 @@ def get_cutoff_timestamp_for_date(date):
   time = eastern_tz.localize(
       datetime.datetime(date.year, date.month, date.day, hour=12))
   return int(time.timestamp()) * 1000
+
+# Structure holding the Sunday of a week and all responses in that week.
+WeeklyResponses = namedtuple('WeeklyResponses', 'date responses')
+
+# Groups survey responses by week and returns a list of WeeklyResponses.
+# Consumes the input list.
+# The returned list is sorted by date in ascending order. Responses in each item
+# is sorted by createdTimestamp in ascending order.
+# If there is no response in a week, a WeeklyResponse with an empty list will
+# still be added to the returned value.
+# Requires:
+#   survey_responses to be sorted by createdTimestamp in ascending order.
+def group_by_week(survey_responses, current_week_start_date):
+  result = []
+  week_start_date = current_week_start_date
+  week_start_timestamp = get_cutoff_timestamp_for_date(week_start_date)
+  week_responses = []
+  while len(survey_responses) > 0:
+    response_timestamp = int(survey_responses[-1]['createdTimestamp'])
+    # This response belongs to the current week.
+    if response_timestamp >= week_start_timestamp:
+      week_responses.append(survey_responses.pop())
+      continue
+    # This response belongs to a previous week. Add the current responses to
+    # result and decrement week_start_date by 7 days.
+    else:
+      # Reverse the list so that it is in ascending order by createdTimestamp.
+      week_responses.reverse()
+      result.append(WeeklyResponses(week_start_date, week_responses))
+      week_start_date -= datetime.timedelta(days=7)
+      week_start_timestamp = get_cutoff_timestamp_for_date(week_start_date)
+      week_responses = []
+  # Add the last responses to result.
+  result.append(WeeklyResponses(week_start_date, week_responses))
+  # Reverse the list so that it is in ascending order by date.
+  result.reverse()
+  return result
--- firestore/real_time_triggers/subscriber/user_report/response_data_util_test.py
+++ firestore/real_time_triggers/subscriber/user_report/response_data_util_test.py
@@ -8,6 +8,7 @@ import datetime
 import unittest
 
 from user_report import response_data_util
+from user_report.response_data_util import WeeklyResponses
 
 class TestComputeScoreSum(unittest.TestCase):
   def test_valid_int(self):
@@ -95,6 +96,86 @@ class TestGetCutoffTimestampForDate(unittest.TestCase):
     date = datetime.date(2020, 11, 1)
     self.assertEqual(response_data_util.get_cutoff_timestamp_for_date(date),
                      1604250000000)
+
+
+class TestGroupByWeek(unittest.TestCase):
+  def test_no_empty_week(self):
+    current_week_start_date = datetime.date(2020, 5, 17)
+    s1 = {
+        'activityId': 'SurveyA',
+        'createdTimestamp': '1589119200000', # 2020-05-10 10am ET
+        'results': [{'key': 'Q1', 'value': '7'}, {'key': 'Q2', 'value': '8'}]}
+    s2 = {
+        'activityId': 'SurveyB',
+        'createdTimestamp': '1589565600000', # 2020-05-15 2pm ET
+        'results': [{'key': 'Q1', 'value': '5'}, {'key': 'Q2', 'value': '6'}]}
+    s3 = {
+        'activityId': 'SurveyA',
+        'createdTimestamp': '1589727600000', # 2020-05-17 11am ET
+        'results': [{'key': 'Q1', 'value': '3'}, {'key': 'Q2', 'value': '4'}]}
+    s4 = {
+        'activityId': 'SurveyA',
+        'createdTimestamp': '1589986800000', # 2020-05-20 11am ET
+        'results': [{'key': 'Q1', 'value': '1'}, {'key': 'Q2', 'value': '2'}]}
+    survey_responses = [s1, s2, s3, s4]
+    expected_result = [
+        WeeklyResponses(datetime.date(2020, 5, 3), [s1]),
+        WeeklyResponses(datetime.date(2020, 5, 10), [s2, s3]),
+        WeeklyResponses(datetime.date(2020, 5, 17), [s4])]
+    self.assertEqual(response_data_util.group_by_week(survey_responses,
+                                                      current_week_start_date),
+                     expected_result)
+  def test_current_week_empty(self):
+    current_week_start_date = datetime.date(2020, 5, 24)
+    s1 = {
+        'activityId': 'SurveyA',
+        'createdTimestamp': '1589119200000', # 2020-05-10 10am ET
+        'results': [{'key': 'Q1', 'value': '7'}, {'key': 'Q2', 'value': '8'}]}
+    s2 = {
+        'activityId': 'SurveyB',
+        'createdTimestamp': '1589565600000', # 2020-05-15 2pm ET
+        'results': [{'key': 'Q1', 'value': '5'}, {'key': 'Q2', 'value': '6'}]}
+    s3 = {
+        'activityId': 'SurveyA',
+        'createdTimestamp': '1589727600000', # 2020-05-17 11am ET
+        'results': [{'key': 'Q1', 'value': '3'}, {'key': 'Q2', 'value': '4'}]}
+    s4 = {
+        'activityId': 'SurveyA',
+        'createdTimestamp': '1589986800000', # 2020-05-20 11am ET
+        'results': [{'key': 'Q1', 'value': '1'}, {'key': 'Q2', 'value': '2'}]}
+    survey_responses = [s1, s2, s3, s4]
+    expected_result = [
+        WeeklyResponses(datetime.date(2020, 5, 3), [s1]),
+        WeeklyResponses(datetime.date(2020, 5, 10), [s2, s3]),
+        WeeklyResponses(datetime.date(2020, 5, 17), [s4]),
+        WeeklyResponses(datetime.date(2020, 5, 24), [])]
+    self.assertEqual(response_data_util.group_by_week(survey_responses,
+                                                      current_week_start_date),
+                     expected_result)
+
+  def test_skip_two_weeks(self):
+    current_week_start_date = datetime.date(2020, 5, 24)
+    s1 = {
+        'activityId': 'SurveyA',
+        'createdTimestamp': '1589119200000', # 2020-05-10 10am ET
+        'results': [{'key': 'Q1', 'value': '7'}, {'key': 'Q2', 'value': '8'}]}
+    s2 = {
+        'activityId': 'SurveyA',
+        'createdTimestamp': '1590339600000', # 2020-05-24 1pm ET
+        'results': [{'key': 'Q1', 'value': '1'}, {'key': 'Q2', 'value': '2'}]}
+    s3 = {
+        'activityId': 'SurveyB',
+        'createdTimestamp': '1590343200000', # 2020-05-24 2pm ET
+        'results': [{'key': 'Q1', 'value': '3'}, {'key': 'Q2', 'value': '4'}]}
+    survey_responses = [s1, s2, s3]
+    expected_result = [
+        WeeklyResponses(datetime.date(2020, 5, 3), [s1]),
+        WeeklyResponses(datetime.date(2020, 5, 10), []),
+        WeeklyResponses(datetime.date(2020, 5, 17), []),
+        WeeklyResponses(datetime.date(2020, 5, 24), [s2, s3])]
+    self.assertEqual(response_data_util.group_by_week(survey_responses,
+                                                      current_week_start_date),
+                     expected_result)
 
 
 if __name__ == '__main__':
