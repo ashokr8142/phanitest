a7d3baf07373547e2c71ab0b9699efb73c492449
malaybtc291 <63813003+malaybtc291@users.noreply.github.com>
2021-01-05 13:06:18 +0530

    Revert "Revert "Prod enhancement 3 registration server""

---
+++ user-registration-server-ws/user-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/beans/SummaryReportBean.java
@@ -0,0 +1,11 @@
+package com.google.cloud.healthcare.fdamystudies.beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SummaryReportBean {
+  private String title;
+  private String content;
+}
